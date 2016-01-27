1e3cbc696ddfddc25c5f3aa19639fbfda3ea070f
Esg450 <evan.gentis@gmail.com>
2016-01-27 15:01:35 -0500

    Added class named after myself

---
+++ Lab2B_Team8/src/lab2b_team8/Evan.java
@@ -0,0 +1,9 @@
+package lab2b_team8;
+
+/**
+ *
+ * @author evangentis
+ */
+public class Evan {
+    
+}
---
+++ Lab2B_Team8/src/lab2b_team8/Lab2B_Team8.java
@@ -0,0 +1,16 @@
+package lab2b_team8;
+
+/**
+ *
+ * @author evangentis
+ */
+public class Lab2B_Team8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
